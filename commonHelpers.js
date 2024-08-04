b6d40d3aa9f0b935159252362e14bd738d8a5b40
Mati220186 <139258171+Mati220186@users.noreply.github.com>
2024-08-04 21:02:51 +0000

    Deploying to gh-pages from @ Mati220186/goit-js-hw-12@694b6dde4809a4db4e213002915abfd5d9733ca6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,18 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{a as g,i}from"./assets/vendor-xN_OkTRX.js";const y=document.querySelector("#searchForm"),l=document.querySelector("#gallery"),a=document.querySelector("#loadMore");let m="",r=1,c=40;y.addEventListener("submit",async e=>{e.preventDefault(),m=document.querySelector("#searchQuery").value.trim(),r=1,l.innerHTML="",a.style.display="none",await p()});a.addEventListener("click",async()=>{r++,await p()});async function p(){const t={key:"45239691-411c9704351f7c72c1a4b78aa",q:m,image_type:"photo",orientation:"horizontal",safesearch:"true",page:r,per_page:c},n="https://pixabay.com/api";try{const s=(await g.get(n,{params:t})).data;s.hits.length>0&&(d(s.hits),console.log(s.hits),a.style.display="block",r*c>=s.totalHits?(a.style.display="none",i.info({title:"Informacja",message:"Were sorry, but you've reached the end of search results.",position:"topRight"})):(a.style.display="none",i.info({title:"Informacja",message:"No images found. Please try a different search query.",position:"topRight"})))}catch{i.info({title:"Informacja",message:"Error fetching images:",position:"topRight"})}}function d(e){const t=document.createDocumentFragment();e.forEach(n=>{const o=document.createElement("img");o.src=n.webformatURL,o.alt=n.tags,t.appendChild(o)}),l.appendChild(t),h()}function h(){const e=document.querySelector("#gallery img");if(e){const{height:t}=e.getBoundingClientRect();window.scrollBy({top:t*2,behavior:"smooth"})}}
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{a as g,i as r,S as h}from"./assets/vendor-0Fq3u7cb.js";const f=document.querySelector("#searchForm"),p=document.querySelector("#gallery"),s=document.querySelector("#loadMore"),c=document.querySelector("#loader");let y="",n=1,d=6;f.addEventListener("submit",async a=>{a.preventDefault(),y=document.querySelector("#searchQuery").value.trim(),n=1,p.innerHTML="",s.style.display="none",await m()});s.addEventListener("click",async()=>{n+=1,c.style.display="none",await m()});async function m(){const o=`https://pixabay.com/api/?key=45239691-411c9704351f7c72c1a4b78aa&q=${encodeURIComponent(y)}&image_type=photo&orientation=horizontal&safesearch=true&page=${n}&per_page=${d}`;c.style.display="flex";try{const e=(await g.get(o)).data;console.log(e),e.hits.length>0?(c.style.display="none",u(e.hits),console.log(e.hits),s.style.display="",n*d>=e.totalHits&&(s.style.display="none",r.info({title:"Informacja",message:"Were sorry, but you've reached the end of search results.",position:"topRight"}))):(s.style.display="none",r.info({title:"Informacja",message:"No images found. Please try a different search query.",position:"topRight"}))}catch{r.info({title:"Error",message:"Error fetching images:",position:"topRight",color:"red"})}}function u(a){a.forEach(t=>{const e=document.createElement("a");e.href=t.largeImageURL,e.dataset.lightbox="gallery",e.dataset.title=t.tags;const l=document.createElement("img");l.src=t.webformatURL,l.alt=t.tags;const i=document.createElement("div");i.classList.add("image-info"),i.innerHTML=`
+            <div>
+                <span class="label">Likes:</span>
+                <span>${t.likes}</span>
+            </div>
+            <div>
+                <span class="label">Views:</span>
+                <span>${t.views}</span>
+            </div>
+            <div>
+                <span class="label">Comments:</span>
+                <span>${t.comments}</span>
+            </div>
+            <div>
+                <span class="label">Downloads:</span>
+                <span>${t.downloads}</span>
+            </div>`,p.appendChild(e),e.appendChild(l),e.appendChild(i)}),v(),new h("#gallery a",{}).refresh()}function v(){const a=document.querySelector("#gallery img");if(a){const{height:o}=a.getBoundingClientRect();window.scrollBy({top:o*3,behavior:"smooth"})}}
 //# sourceMappingURL=commonHelpers.js.map
